131978e89ae0faf3bce36a545f48ebb86fc5dec7
Owen Chiu <q0972016679@gmail.com>
2019-04-27 23:26:18 +0900

    calculate comment average

--- read.py
+++ read.py
@@ -4,7 +4,9 @@ with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
 		count += 1   # count = count + 1
-		if count % 100000 == 0:
+		if count % 10000 == 0:
 			print(len(data))
-							
-			
+sum_len = 0
+for d in data :
+	sum_len = sum_len + len(d)
+print('留言平均長度為',sum_len / len(data),'個字')
