c936b82f3ad75d1b5365d47e4c30dec42b1b4069
Owen Chiu <q0972016679@gmail.com>
2019-04-29 23:09:17 +0900

    篩選留言長度

--- read.py
+++ read.py
@@ -9,6 +9,17 @@ with open('reviews.txt', 'r') as f:
 			print(line)
 
 sum_len = 0
-for d in data :
+for d in data :    #d:每筆留言  data:裝著留言的清單
 	sum_len = sum_len + len(d)
-print('留言平均長度為',sum_len / len(line),'個字')
+print('留言平均長度為',sum_len / len(data),'個字')
+
+new = []
+new_1 = []
+for d in data:
+	if len(d) < 100:   
+		new.append(d)   #把長度<100的留言裝進清單"new = [ ]"裡面
+	
+
+print(len(new))
+print('共有',len(new),'筆資料長度小於100')
+
