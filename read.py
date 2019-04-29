266118a2c64e35cbe8598423d2419ecc82209fb5
Owen Chiu <q0972016679@gmail.com>
2019-04-29 22:08:55 +0900

    print every 100000 comment

--- read.py
+++ read.py
@@ -4,9 +4,11 @@ with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
 		count += 1   # count = count + 1
-		if count % 10000 == 0:
+		if count % 100000 == 0:
 			print(len(data))
+			print(line)
+
 sum_len = 0
 for d in data :
 	sum_len = sum_len + len(d)
-print('留言平均長度為',sum_len / len(data),'個字')
+print('留言平均長度為',sum_len / len(line),'個字')
